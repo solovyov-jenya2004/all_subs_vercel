3b3dc69532ba4a503947b8209c766e6072f438e6
solovyov-jenya2004 <278155303+solovyov-jenya2004@users.noreply.github.com>
2026-05-24 17:48:48 +0300

    Update final_sorted.js

--- api/final_sorted.js
+++ api/final_sorted.js
@@ -1,7 +1,6 @@
 export default async function handler(req, res) {
   const GITHUB_URL = 'https://raw.githubusercontent.com/solovyov-jenya2004/all_subs/main/final_sorted';
   try {
-    // Встроенный fetch доступен глобально
     const response = await fetch(GITHUB_URL);
     if (!response.ok) {
       res.status(502).send(`GitHub error: ${response.status}`);
@@ -9,12 +8,10 @@ export default async function handler(req, res) {
     }
     const body = await response.text();
 
-    // Устанавливаем заголовки
     res.setHeader('Content-Type', 'text/plain; charset=utf-8');
     res.setHeader('Subscription-Userinfo', 'upload=0; download=0; total=0');
-    res.setHeader('Cache-Control', 'public, max-age=60');
+    res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');   // <-- без кэша
     res.setHeader('Content-Disposition', "inline; filename=\"all_subs\"; filename*=UTF-8''%F0%9F%9A%80%20all_subs");
-
 
     res.status(200).send(body);
   } catch (error) {
